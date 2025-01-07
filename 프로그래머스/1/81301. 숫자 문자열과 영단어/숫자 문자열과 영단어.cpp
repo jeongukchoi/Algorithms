4b8b4156723e82d9224f881990f1d352e03c3e6b
jeongukchoi <32578688+jeongukchoi@users.noreply.github.com>
2025-01-07 10:23:59 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 int solution(string s) {
     long long answer = 0;
-    vector<int> numbers;
     
     unordered_map<string, int> wordNum = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
     
@@ -16,33 +15,30 @@ int solution(string s) {
         // iterate and check
         if (isdigit(s[i])) {
             int num = s[i] - '0';
-            numbers.push_back(num);
+            answer = answer * 10 + num;
         } else {
             auto it = wordNum.find(s.substr(i, 3));
             if (it != wordNum.end()) {
-                numbers.push_back(it->second);
+                answer = answer * 10 + it->second;
                 i += 2;
                 continue;
             }
 
             it = wordNum.find(s.substr(i, 4));
             if (it != wordNum.end()) {
-                numbers.push_back(it->second);
+                answer = answer * 10 + it->second;
                 i += 3;
                 continue;
             }
 
             it = wordNum.find(s.substr(i, 5));
             if (it != wordNum.end()) {
-                numbers.push_back(it->second);
+                answer = answer * 10 + it->second;
                 i += 4;
                 continue;
             }
         }
     }
-    
-    for (int i = 0; i < numbers.size(); i++)
-        answer += numbers[numbers.size() - 1 - i] * pow(10, i);
-    
+
     return answer;
 }
