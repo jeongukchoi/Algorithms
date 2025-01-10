9a1b048454c1fc68a8374a744fd20b41c1f25d58
jeongukchoi <32578688+jeongukchoi@users.noreply.github.com>
2025-01-10 09:45:47 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -4,16 +4,13 @@
 using namespace std;
 
 string solution(int a, int b) {
-    const vector<int> monthToDays({0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335});
+    const vector<int> daysInMonth({31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30});
     const vector<string> whatDay({"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"});
     
-    int deltaDays = monthToDays[a - 1] + b - 1;
-    auto it = whatDay.begin() + 4;
-    for (int i = 0; i < deltaDays % 7; i++)
-    {
-        it++;
-        if (it == whatDay.end()) it = whatDay.begin();
+    int totalDays = b - 1;
+    for (int i = 0; i < a - 1; i++) {
+        totalDays += daysInMonth[i];
     }
     
-    return *it;
+    return whatDay[(totalDays + 4) % 7];
 }
