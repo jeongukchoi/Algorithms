bbbfb50eabadc95ccb8f156b151120ce247ecf62
jeongukchoi <32578688+jeongukchoi@users.noreply.github.com>
2025-01-17 20:10:47 +0900

    [level 1] Title: 옹알이 (2), Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -0,0 +1,86 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> babbling) {
+    int answer = 0;
+    
+    vector<string> cases = {"aya", "ye", "woo", "ma"};
+    
+    for (string s : babbling) {
+        // 길이가 1이면 패스
+        if (s.length() < 2) {
+            continue;
+        }
+        
+        // 길이가 2 또는 3인 경우 일치하면 카운트
+        if (s.length() == 2 || s.length() == 3) {
+            for (string s2 : cases) {
+                if (s == s2) {
+                    answer++;
+                    break;
+                }
+            }
+            continue;
+        }
+                
+        string temp = s;
+        string last = "";
+        bool flag = false;
+        while (true) {
+            // 남은 글자가 없으면 OK
+            if (temp.empty()) {
+                flag = true;
+                break;
+            }
+        
+            // 1 글자 남았으면 X
+            if (temp.length() == 1) {
+                break;
+            }
+            
+            // 첫 2글자와 비교
+            string FirstTwo = temp.substr(0, 2);
+            
+            // 마지막 발음과 같으면 X
+            if (FirstTwo == last) break;
+            
+            // 발음할 수 있는지 체크
+            if (FirstTwo == "ye") {
+                temp = temp.substr(2);
+                last = "ye";
+                continue;
+            }
+            else if (FirstTwo == "ma") {
+                temp = temp.substr(2);
+                last = "ma";
+                continue;
+            }
+            
+            // 일치하지 않았고 마지막 2글자를 확인한 상태
+            if (temp.length() == 2) break;
+                        
+            // 3 글자 이상 남음
+            string FirstThree = temp.substr(0, 3);
+            if (FirstThree == last) break;
+            
+            if (FirstThree == "aya") {
+                temp = temp.substr(3);
+                last = "aya";
+                continue;
+            } else if (FirstThree == "woo") {
+                temp = temp.substr(3);
+                last = "woo";
+                continue;
+            }
+            
+            // 전부 일치하지 않았음
+            break;
+        }
+        
+        if (flag) answer++;
+    }
+    
+    return answer;
+}
