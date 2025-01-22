90669d337717d085e568c754f5411db1dd60f7f6
jeongukchoi <32578688+jeongukchoi@users.noreply.github.com>
2025-01-22 09:38:31 +0900

    [level 1] Title: 문자열 나누기, Time: 19.38 ms, Memory: 25.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int recursion(string s) {
+    if (s.length() <= 1) return 1;
+    
+    char x = s[0];
+    int same = 0;
+    int different = 0;
+
+    for (int i = 0; i < s.length() - 1; i++) {
+        if (s[i] == x) same++;
+        else different++;
+        
+        if (same > 0 && same == different)
+            return 1 + recursion(s.substr(i+1));
+    }
+    return 1;
+}
+
+int solution(string s) {
+    return recursion(s);;
+}
