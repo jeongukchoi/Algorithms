049a5356d5a9a55d3516eab756bf47452d47c18f
jeongukchoi <32578688+jeongukchoi@users.noreply.github.com>
2025-02-05 18:13:06 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -3,13 +3,11 @@ using namespace std;
 int solution(int n, int a, int b)
 {
     int answer = 0;
-    while (true)
+    while (a != b)
     {
-        answer++;
-        // a 가 짝수이면 b 는 a -1 / b 가 짝수이면 a 는 b - 1
-        if ((a % 2 == 0 && b == a - 1) || (b % 2 == 0 && a == b - 1)) break;
         a = (a + (a % 2)) / 2;
         b = (b + (b % 2)) / 2;
+        answer++;
     }
     return answer;
 }
