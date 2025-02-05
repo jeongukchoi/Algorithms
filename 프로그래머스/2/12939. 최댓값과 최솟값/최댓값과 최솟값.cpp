2ae4162529cc56fbbe49c3ad692862fc652cb057
jeongukchoi <32578688+jeongukchoi@users.noreply.github.com>
2025-02-05 12:30:11 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,23 +1,30 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <limits.h>
+#include <iostream>
 
 using namespace std;
 
 string solution(string s) {
-    size_t start = -1;
-    string curr = s;
-    int currMax = INT_MIN;
-    int currMin = INT_MAX;
     
-    do {
-        curr = curr.substr(start + 1);
-        int currInt = stoi(curr);
-        currMax = max(currMax, currInt);
-        currMin = min(currMin, currInt);
-        start = curr.find(" ");
-    } while (start != string::npos && start < curr.length() - 1);
+    string temp = "";
+    vector<int> v;
     
-    return to_string(currMin) + " " + to_string(currMax);
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            v.push_back(stoi(temp));
+            temp.clear();
+            continue;
+        }
+        
+        temp += s[i];
+    }
+    
+    if (!temp.empty()) v.push_back(stoi(temp));
+    
+    sort(v.begin(), v.end());
+    
+    return to_string(*v.begin()) + " " + to_string(*(v.end()-1));
 }
