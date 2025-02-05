c7b792cc6174d25545ceadea14f53aaca126321d
jeongukchoi <32578688+jeongukchoi@users.noreply.github.com>
2025-02-05 11:27:33 +0900

    [level 1] Title: 신고 결과 받기, Time: 158.87 ms, Memory: 49 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    vector<int> answer;
+    
+    unordered_set<string> reportSet;
+    for (const string& s : report) reportSet.insert(s);
+    
+    unordered_map<string, vector<string>> reportMap;
+    unordered_map<string, int> reportCountMap;
+    for (const string& s : reportSet)
+    {
+        size_t pos = s.find(" ");
+        string reporter = s.substr(0, pos);
+        string reported = s.substr(pos + 1);
+        
+        reportMap[reported].push_back(reporter);
+        reportCountMap[reported] += 1;
+    }
+    
+    unordered_map<string, int> mailCountMap;
+    for (const auto& reportCount : reportCountMap)
+    {
+        if (reportCount.second >= k)
+        {
+            for (const string& reporter : reportMap[reportCount.first])
+            {
+                mailCountMap[reporter] += 1;
+            }
+        }
+    }
+    
+    for (string id : id_list) answer.push_back(mailCountMap[id]);
+    
+    return answer;
+}
