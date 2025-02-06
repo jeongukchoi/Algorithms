8b045fb64eea620bdc8abdf89fc38063a7a9b619
jeongukchoi <32578688+jeongukchoi@users.noreply.github.com>
2025-02-06 10:04:27 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -1,50 +1,34 @@
 #include <vector>
-#include <set>
 #include <algorithm>
-#include <numeric>
-#include <cmath>
 
 using namespace std;
+
+int GCD(int a, int b) {
+    int m = max(a, b);
+    int n = min(a, b);
+    
+    while (m % n != 0)
+    {
+        int r = m % n;
+        m = n;
+        n = r;
+    }
+    return n;
+}
 
 int solution(vector<int> arr) {
     int answer = 0;
     
-    // 소인수분해
-    set<int> primeFactors;
-    for (int n : arr)
+    if (arr.size() < 2) return arr[0];
+    
+    // 최소공배수 = 두 수의 곱 / 최대공약수
+    int a = arr[0];
+    for (int i = 1; i < arr.size(); i++)
     {
-        for (int i = 2; i <= sqrt(n); i++)
-        {
-            while (n % i == 0)
-            {
-                primeFactors.insert(i);
-                n /= i;
-            }
-        }
-        if (n > 1) primeFactors.insert(n);
+        int b = arr[i];
+        int LCM = a * b / GCD(a, b);
+        a = LCM;
     }
     
-    // product = 중복되지 않는 소인수들의 곱
-    int product = accumulate(primeFactors.begin(), primeFactors.end(), 1, multiplies<int>());
-    
-    // 최소공배수 = product * i
-    int i = 1;
-    while (true)
-    {
-        bool flag = true;
-        for (int n : arr)
-        {
-            if (product * i % n != 0)
-            {
-                flag = false;
-            }
-        }
-        if (flag) {
-            answer = product * i;
-            break;
-        }
-        i++;
-    }
-    
-    return answer;
+    return a;
 }
