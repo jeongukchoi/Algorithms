86496cb323963511d7218865a9f7666f9f787444
jeongukchoi <32578688+jeongukchoi@users.noreply.github.com>
2025-04-24 10:09:45 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,36 +1,30 @@
-#include <string>
-#include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     
-    vector<int> remainingDays;
+    queue<int> q;
     for (int i = 0; i < progresses.size(); ++i)
     {
         int remainderCheck = (100 - progresses[i]) % speeds[i] > 0;
-        remainingDays.push_back((100 - progresses[i]) / speeds[i] + remainderCheck);
+        q.push((100 - progresses[i]) / speeds[i] + remainderCheck);
     }
 
-    for (int i = 0; i < remainingDays.size(); )
+    while (!q.empty())
     {
-        int days = remainingDays[i];
-        for (int j = i; j < remainingDays.size(); ++j)
-        {
-            cout << "subtract " << remainingDays[i] << " from " << remainingDays[j] << endl;
-            remainingDays[j] -= days;
-        }
-        
-        int count = 0;
-        for (int j = i; j < remainingDays.size(); ++j)
-        {
-            if (remainingDays[j] > 0) break;
+        int count = 1;
+        int curr = q.front();
+        q.pop();
+
+        while (curr >= q.front() && !q.empty()) {
+            q.pop();
             count++;
         }
+
         answer.push_back(count);
-        i += count;
     }
     
     return answer;
